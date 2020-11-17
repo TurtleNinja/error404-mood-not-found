12968f9e740d87b1fa451c3d6575beb917321ce4
cmiciano <35476727+cmiciano@users.noreply.github.com>
2020-11-16 16:38:08 -0800

    removed mood_rand_home function

--- flaskr/app.py
+++ flaskr/app.py
@@ -16,20 +16,10 @@ def login():
 def signup():
     return render_template("SignUpPage.html")
 
-<<<<<<< HEAD
-@app.route('/mood_randomizer_home')
-def mood_rand_home():
-    return render_template("moodrandhome.html")    
-
 
 @app.route('/mood_randomizer')
 def mood_randomizer():
     return render_template("mood.html")
-=======
-@app.route('/mood_tracker')
-def mood_tracker():
-    return render_template("moodtracker.html")
->>>>>>> d0963f3e1c74ebd2f2290e86fead61dc0f40db32
 
 @app.route('/mood_rating')
 def mood_rate():
