450b4f17344a7d8bc60f4f03a2f18cae75fa96fd
fidwu <fidellapwu@gmail.com>
2020-11-28 11:55:03 -0800

    added linking for mood randomizer dropdown

--- flaskr/static/scripts/linking.js
+++ flaskr/static/scripts/linking.js
@@ -5,4 +5,12 @@ $(document).ready(function() {
     $('.add_entry').click(function(){
         window.location.href = '/mood_rating';
     });
+    $('.rand_submit').click(function(){
+        console.log("submit clicked");
+        var dropdown_val = $('.selectpicker').val();
+        link_str = "mood_randomizer_" + dropdown_val;
+        alert(link_str);
+        console.log(link_str);
+        window.location.href = '/' + link_str;
+    });
 });
