eb14c8813a924a610277a09b7d54179cb592cf04
fidwu <fidellapwu@gmail.com>
2020-12-06 13:56:39 -0800

    change db to app.db

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -5,7 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config.from_object(Config)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///entries.sqlite3'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///entries.sqlite3'
 
 db = SQLAlchemy(app)
 # migrate = Migrate(app, db)
